500486b145b2a5ce3352194d8f0d90889b39cc72
Elena Ru <elenaru@hotmail.de>
2021-02-23 19:28:17 +0100

    Iteration 13 edited

--- src/components/FaceBook.js
+++ src/components/FaceBook.js
@@ -7,12 +7,14 @@ function FaceBook({data}){
         
         return(
            
-              <div key={index}>
-                            <img style={{width:'100px'}} src={person.img} alt="faceBook" />
-                            <p>First Name: {person.firstName} </p>
-                            <p>Last Name: {person.lastName} </p>
-                            <p>Country: {person.country} </p>
-                            <p>Type: {person.isStudent?'student':'teacher'} </p>
+              <div key={index} className="faceBook-Box" style={{border: "1px solid black"}}>
+                            <img style={{width:'100px', marginRight:'20px'}} src={person.img} alt="faceBook" />
+                            <div>
+                                <p>First Name: {person.firstName} </p>
+                                <p>Last Name: {person.lastName} </p>
+                                <p>Country: {person.country} </p>
+                                <p>Type: {person.isStudent?'Student':'Teacher'} </p>
+                            </div>
               </div>
             
           )
